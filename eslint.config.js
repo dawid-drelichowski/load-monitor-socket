575b11444658f76bdd0ae5abbad6ba5cb1e40419
Dawid Drelichowki <dawid.drelichowski@gmail.com>
2025-08-11 19:28:45 +0200

    Change of bracket spacing formatting

--- eslint.config.js
+++ eslint.config.js
@@ -2,23 +2,27 @@ import js from '@eslint/js';
 import globals from 'globals';
 import json from '@eslint/json';
 import markdown from '@eslint/markdown';
-import {defineConfig, globalIgnores} from 'eslint/config';
+import { defineConfig, globalIgnores } from 'eslint/config';
 import prettierConfig from 'eslint-config-prettier';
 
 export default defineConfig([
   globalIgnores(['package-lock.json']),
   prettierConfig,
-  {files: ['**/*.{js,mjs,cjs}'], plugins: {js}, extends: ['js/recommended']},
-  {files: ['**/*.{js,mjs,cjs}'], languageOptions: {globals: globals.node}},
+  {
+    files: ['**/*.{js,mjs,cjs}'],
+    plugins: { js },
+    extends: ['js/recommended'],
+  },
+  { files: ['**/*.{js,mjs,cjs}'], languageOptions: { globals: globals.node } },
   {
     files: ['**/*.json'],
-    plugins: {json},
+    plugins: { json },
     language: 'json/json',
     extends: ['json/recommended'],
   },
   {
     files: ['**/*.md'],
-    plugins: {markdown},
+    plugins: { markdown },
     language: 'markdown/gfm',
     extends: ['markdown/recommended'],
   },
